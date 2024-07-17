688e5d562c892373d3ada349f6e834c41196d00f
Natnael Alemayehu <se.natnael.alemayehu@gmail.com>
2024-07-17 16:55:11 +0300

    hover image not working. Fixing the issue

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,5 +1,8 @@
 import { Section } from "./Section";
 import Heading from "./Heading";
+import { benefits } from "../constants";
+import Arrow from "../assets/svg/Arrow";
+import { GradientLight } from "./design/Benefits";
 
 function Benefits() {
     return (
@@ -9,6 +12,60 @@ function Benefits() {
                     className="md:max-w-md lg:max-w-2xl"
                     title="Chat smarter, Not Harder With Brainwave"
                 />
+                <div className="flex flex-wrap gap-10 mb-10">
+                    {benefits.map((item) => {
+                        console.log(item);
+                        return (
+                            <div
+                                className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
+                                style={{
+                                    backgroundImage: `url(${item.backgroundUrl})`,
+                                }}
+                                key={item.id}
+                            >
+                                <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
+                                    <h5 className="h-2 mb-5"> {item.title} </h5>
+                                    <p className="body-2 mb-6 text-n-3">
+                                        {item.text}
+                                    </p>
+                                    <div className="flex items-center mt-auto">
+                                        <img
+                                            src={item.iconUrl}
+                                            width={48}
+                                            height={48}
+                                            alt={item.title}
+                                        />
+                                        <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">
+                                            Explore more{" "}
+                                        </p>
+                                        <Arrow />
+
+                                        {item.light && <GradientLight />}
+
+                                        <div
+                                            className="absolute inset-[0.5] bg-n-8"
+                                            style={{
+                                                clipPath: "url(#benefits)",
+                                            }}
+                                        >
+                                            <div className="absolute inset-0 opacity-0 transition-opacity hover:opacity-10">
+                                                {item.imageUrl && (
+                                                    <img
+                                                        src={item.iconUrl}
+                                                        width={380}
+                                                        height={362}
+                                                        alt={item.title}
+                                                        className="w-full h-full object-cover"
+                                                    />
+                                                )}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
         </Section>
     );
